410dfcea08f6d8f091d6d52ae02ffbb4db9a571b
unknown <you@example.com>
2019-07-04 20:14:08 +0300

    gg

--- js/main.js
+++ js/main.js
@@ -22,39 +22,54 @@
   $(window).scroll( function () {	
   if ($(window). scrollTop () >= 600){$(".sccroll-top i").fadeIn(400)}
   else ($(".sccroll-top i").fadeOut(400) )
-  
      });
-     var signInPopup =document.querySelector(".popup");
-     var signUpPopup =document.querySelector(".popup2");
-     var signInBottun = document.querySelectorAll("#signIn");
-     var signUpBottun = document.querySelectorAll("#signUp");
-     
-     signInPopup.addEventListener('click', function() {
-        this.style.display = "none";
-     })
-     document.querySelector(".login-popup").addEventListener('click',(el)=>{
-      el.stopPropagation()
-    })
 
-     signUpPopup.addEventListener('click', function() {
-      this.style.display = "none";
+     $(window).scroll( function () {	
+      if ($(window). scrollTop () >= 100){
+        $(".fixed-nav").addClass('my-fixed-nav')
+      }else{
+        $(".fixed-nav").removeClass('my-fixed-nav')
+      }
+      
+         });
+
+
+    //  oppups function
+     var popups =function() {
+      var signInPopup =document.querySelector(".popup");
+      var signUpPopup =document.querySelector(".popup2");
+      var signInBottun = document.querySelectorAll("#signIn");
+      var signUpBottun = document.querySelectorAll("#signUp");
+      
+      signInPopup.addEventListener('click', function() {
+         this.style.display = "none";
       })
-   document.querySelector(".login-popup2").addEventListener('click',(el)=>{
-    el.stopPropagation()
-  })
-
-     signInBottun.forEach((el)=>{
+      document.querySelector(".login-popup").addEventListener('click',(el)=>{
+       el.stopPropagation()
+     })
+ 
+      signUpPopup.addEventListener('click', function() {
+       this.style.display = "none";
+       })
+    document.querySelector(".login-popup2").addEventListener('click',(el)=>{
+     el.stopPropagation()
+   })
+ 
+      signInBottun.forEach((el)=>{
+        el.addEventListener("click",(e)=>{
+          e.preventDefault();
+         signInPopup.style.display = "block"
+         console.log(signInPopup)
+        })
+      })
+ 
+      signUpBottun.forEach((el)=>{
        el.addEventListener("click",(e)=>{
          e.preventDefault();
-        signInPopup.style.display = "block"
+         signUpPopup.style.display = "block"
         console.log(signInPopup)
        })
      })
 
-     signUpBottun.forEach((el)=>{
-      el.addEventListener("click",(e)=>{
-        e.preventDefault();
-        signUpPopup.style.display = "block"
-       console.log(signInPopup)
-      })
-    })
+     }
+     popups();

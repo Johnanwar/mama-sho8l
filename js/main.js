afe5b1ede0e66f969107046394a1468fec20d176
unknown <you@example.com>
2019-08-13 07:24:42 +0300

    gg

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-
+$("#bzoom").zoom({
+	zoom_area_width: 300,
+    autoplay_interval :3000,
+    small_thumbs : 4,
+    autoplay : false
+});
   //  scroll button
   function scrollAndAnimate(){
     $(".sccroll-top i").click (function() {
